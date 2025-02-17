418c06f31515dfc736114a3f9d6e88b5b4e686ac
musiked7 <95246508+musiked7@users.noreply.github.com>
2025-02-17 12:28:52 -0600

    finished exercices 1.10.10

--- practice-java-project/Package1/Person1.java
+++ practice-java-project/Package1/Person1.java
@@ -1,14 +1,22 @@
 package Package1;
 
 public class Person1 {
-  String name = "Eliora";
-  static int age = 5;
+  private String name;
+  private int age;
+
+  public Person1(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
   int counter = 0;
 
   public void printName() {
     String demoMessage = "demo";
     while(counter < 5) {
-      System.out.println("demo message inside loop is: " + demoMessage);
+      demoMessage = "changed message";
+      System.out.println("demo message inside loop is: " + 
+      demoMessage + " and counter is: " + counter + " and name is: " + name + " and age is: " + age);
       counter++;
     }
 
@@ -16,9 +24,7 @@ public class Person1 {
   }
 
   public static void main(String[] args) {
-    Person1 person = new Person1();
-    person.printName();
-   
+    Person1 person1 = new Person1("Anna", 10);
+    person1.printName();
   }
- 
 }
